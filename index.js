156e17bd64cea0dda1ea5194900ea8a6cfed1bd3
Aeranyra <yasyastar38@gmail.com>
2026-06-20 12:10:10 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import { Client, GatewayIntentBits, Collection } from 'discord.js';
 import dotenv from 'dotenv';
+import http from 'http';
 import { registerCommands } from './events/ready.js';
 import { handleInteraction } from './events/interactionCreate.js';
 dotenv.config();
+
+// Tiny web server so Render's free tier sees this as "alive"
+http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('Bot is alive');
+}).listen(process.env.PORT || 3000);
 
 const client = new Client({
     intents: [
